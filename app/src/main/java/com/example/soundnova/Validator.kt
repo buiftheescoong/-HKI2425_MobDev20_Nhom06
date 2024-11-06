8b8f84646972ff4b52e6ab0e2ab0341226863900
Cong <125168053+buiftheescoong@users.noreply.github.com>
2024-11-07 00:24:18 +0700

    handle logic validator

--- app/src/main/java/com/example/soundnova/Validator.kt
+++ app/src/main/java/com/example/soundnova/Validator.kt
@@ -5,12 +5,41 @@ class Validator {
 //        "valid"
 //        "invalid"
 //        "empty"
-        return ""
+        if (email.isNotEmpty()) {
+            if (isValidEmail(email)) {
+                return "valid";
+            }
+            return "invalid";
+        }
+        return "empty"
     }
+
+    fun isValidEmail(email: String): Boolean {
+        val emailRegex = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$"
+        return Regex(emailRegex).matches(email)
+    }
+
 
     fun checkPassword(password: String) : String{
-        return ""
+        if (password.isNotEmpty()) {
+            if (isValidPassword(password)) {
+                return "valid"
+            }
+            return "invalid"
+        }
+        return "empty"
     }
 
+    fun isValidPassword(password: String): Boolean {
+        val passwordRegex = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@\$!%*?&])[A-Za-z\\d@\$!%*?&]{8,}$"
+        return Regex(passwordRegex).matches(password) && !password.contains(" ")
+    }
 
+}
+
+fun main() {
+    val email : String = "user@gmail.com"
+    val validator = Validator()
+
+    println(validator.isValidEmail(email))
 }
