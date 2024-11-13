ff8751ab172821a84a872cacc75cd79cbc504475
MinhPhuoc3504 <124911496+MinhPhuoc3504@users.noreply.github.com>
2024-11-14 03:21:52 +0700

    tao API cho album, du lieu ve album, nhac si, the loai

---
+++ app/src/main/java/com/example/soundnova/models/AlbumDetailsResponse.kt
@@ -0,0 +1,47 @@
+package com.example.soundnova.models
+
+import com.google.gson.annotations.SerializedName
+
+data class AlbumDetailsResponse(
+    @SerializedName("tracks")
+    val tracks: Tracks,
+
+    @SerializedName("release_date")
+    val releaseDate: String,
+
+    var albumName: String,
+
+    var albumPicture: String
+)
+
+data class Tracks(
+    @SerializedName("data")
+    val data: List<TrackData>
+)
+
+class TrackData(
+    @SerializedName("id")
+    val id: Int,
+
+    @SerializedName("title")
+    val title: String,
+
+    @SerializedName("duration")
+    val duration: Int,
+
+    @SerializedName("preview")
+    val preview: String,
+
+    var isLiked: Boolean = false,
+){
+    //var backgroundColor by  mutableStateOf(Color.DarkGray)
+    fun getDurationInMinutes(): String{
+        val minutes = duration / 60
+        val seconds = duration % 60
+        return "$minutes:$seconds"
+    }
+    fun getTitleDeletingAfterEncounterParentheses(): String{
+        val index = title.indexOf("(")
+        return if(index == -1) title else title.substring(0, index)
+    }
+}
