30f120f252c98e9378fb179443a5ba9f222b1c70
Cong <125168053+buiftheescoong@users.noreply.github.com>
2024-11-14 15:41:12 +0700

    fix song adapter

--- app/src/main/java/com/example/soundnova/Song.kt
+++ app/src/main/java/com/example/soundnova/Song.kt
@@ -1,7 +1,11 @@
 package com.example.soundnova
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Song(
     val name: String,
     val artist: String,
     val imageUrl: String
-)
+) : Parcelable
