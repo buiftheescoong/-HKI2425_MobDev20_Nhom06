0877a0cc832981e6f7e598a899bf299a26980cd5
hoamai1509 <hoamai1509@gmail.com>
2024-11-21 14:03:18 +0700

    update home_activity.xml library.xml

--- app/src/main/java/com/example/soundnova/HomeActivity.kt
+++ app/src/main/java/com/example/soundnova/HomeActivity.kt
@@ -3,41 +3,39 @@ package com.example.soundnova
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
 import android.widget.ImageView
-import androidx.appcompat.app.AppCompatActivity
+import androidx.activity.ComponentActivity
 import androidx.lifecycle.lifecycleScope
-import androidx.navigation.fragment.NavHostFragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.soundnova.databinding.ActivityMainBinding
 import com.example.soundnova.models.Tracks
 import com.example.soundnova.service.DeezerApiHelper
 import kotlinx.coroutines.launch
 
-class HomeActivity : AppCompatActivity() {
-//    private lateinit var recentRecyclerView: RecyclerView
-//    private lateinit var favoriteRecyclerView: RecyclerView
-//    private lateinit var btnsetting:ImageView
+class HomeActivity : ComponentActivity() {
+    private lateinit var recommendRecyclerView: RecyclerView
+    private lateinit var recentRecyclerView: RecyclerView
+    private lateinit var popularAlbumsView: RecyclerView
+    private lateinit var favoriteArtistsView: RecyclerView
+    private lateinit var btnsetting: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityMainBinding.inflate(LayoutInflater.from(this))
-        setContentView(binding.root)
-        Log.d("Tag", "1")
+        setContentView(R.layout.home_activity)
 
-//
-//        recentRecyclerView = findViewById(R.id.recyclerViewTabsSongs)
-//        favoriteRecyclerView = findViewById(R.id.recyclerViewFavoriteSongs)
-//        btnsetting = findViewById(R.id.buttonSettings)
+        recommendRecyclerView = findViewById(R.id.recyclerViewRecommendSongs)
+        recentRecyclerView = findViewById(R.id.recyclerViewRecentSongs)
+        popularAlbumsView = findViewById(R.id.recyclerViewPopularAlbums)
+        favoriteArtistsView = findViewById(R.id.recyclerViewFavoriteArtists)
+        btnsetting = findViewById(R.id.buttonSettings)
 
-//        btnsetting.setOnClickListener {
-//            startActivity(Intent(applicationContext, Setting::class.java))
-//        }
+        btnsetting.setOnClickListener {
+            startActivity(Intent(applicationContext, Setting::class.java))
+        }
 
-//        lifecycleScope.launch {
-//            loadSongs()
-//        }
+        lifecycleScope.launch {
+            loadSongs()
+        }
 
 //        SpotifyService.getAccessToken { token ->
 //            if (token != null) {
@@ -46,17 +44,25 @@ class HomeActivity : AppCompatActivity() {
 //        }
     }
 
-//    private suspend fun loadSongs() {
-//        val deezerApiHelper = DeezerApiHelper
-//        val popularTracks : Tracks = deezerApiHelper.fetchPopularTracks()
-//        Log.d("TAG", "0")
-//        runOnUiThread {
-//            recentRecyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
-//            recentRecyclerView.adapter = SongAdapter(popularTracks)
-//        }
-//        runOnUiThread {
-//            favoriteRecyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
-//            favoriteRecyclerView.adapter = SongAdapter(popularTracks)
-//        }
-//    }
+    private suspend fun loadSongs() {
+        val deezerApiHelper = DeezerApiHelper
+        val popularTracks : Tracks = deezerApiHelper.fetchPopularTracks()
+        Log.d("TAG", "0")
+        runOnUiThread {
+            recommendRecyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+            recommendRecyclerView.adapter = SongAdapter(popularTracks)
+        }
+        runOnUiThread {
+            recentRecyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+            recentRecyclerView.adapter = SongAdapter(popularTracks)
+        }
+        runOnUiThread {
+            popularAlbumsView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+            popularAlbumsView.adapter = SongAdapter(popularTracks)
+        }
+        runOnUiThread {
+            favoriteArtistsView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+            favoriteArtistsView.adapter = SongAdapter(popularTracks)
+        }
+    }
 }
